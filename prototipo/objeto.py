5c7cc150578df669cda1cdc6423447b2f05c7bd4
FKettl <fekettl@gmail.com>
2022-01-12 18:57:49 -0300

    Adiciona inicio da classe Obstaculo (incompleto)

---
+++ prototipo/objeto.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Obstaculo(pygame.sprite.Sprite):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.surface = 'image()'
+        self.rect = 'get_rect()'
