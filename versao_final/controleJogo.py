aff8074c6643d9b48f85f0460eca43d374365f11
sammycosta <arminetah@hotmail.com>
2022-03-10 21:22:27 -0300

    Interface menu principal com botões clicáveis

--- versao_final/controleJogo.py
+++ versao_final/controleJogo.py
@@ -10,8 +10,9 @@ from menuView import menuView
 from skin import Skin
 from updater import Updater
 
+
 class ControleJogo():
-    
+
     def __init__(self):
         self.__jogador = Jogador()
         self.__skins = [Skin('Quadrado Preto', 'quadrado preto.png'),
@@ -47,24 +48,36 @@ class ControleJogo():
         FPS = 60
         clock = pygame.time.Clock()
         while True:
+            mouse_pos = pygame.mouse.get_pos()
+            detecta_mouse = self.__menu_view.desenha(mouse_pos)
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.display.quit()
                     pygame.quit()
                     exit()
                 if event.type == KEYDOWN:
-                    if event.key == pygame.K_TAB:
-                        return self.iniciar_partida()
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.display.quit()
-                        pygame.quit()
-                        exit()
                     if event.key == pygame.K_s:
                         if self.__jogador.skin_atual.nome == 'Padrão':
                             self.__jogador.muda_skin(choice(self.__skins))
                         else:
                             self.__jogador.muda_skin(Skin('Padrão', 'geo.png'))
-            self.__menu_view.desenha()
+
+                # detectao de cliques
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for botao in self.__menu_view.lista_botoes:
+                        if botao.detecta_mouse(mouse_pos):
+                            detecta_mouse = botao.detecta_mouse(
+                                mouse_pos), botao
+                            break
+                    if detecta_mouse is not None:
+                        if detecta_mouse[0]:
+                            if detecta_mouse[1].mensagem == 'Jogar':
+                                return self.iniciar_partida()
+                            elif detecta_mouse[1].mensagem == 'Sair':
+                                pygame.display.quit()
+                                pygame.quit()
+                                exit()
+
             clock.tick(FPS)
 
     def iniciar_partida(self):
@@ -104,15 +117,18 @@ class ControleJogo():
                 self.partida.desenha_fim_de_jogo()
 
             else:
+
                 if keys_pressed[pygame.K_SPACE]:
-                    self.jogador.pular()
+                    if self.jogador.nochao:
+                        self.jogador.pular()
 
                 # self.partida.tela.blit(bg_surface, (0, 0))
                 self.partida.draw_bg()
                 self.partida.desenhar_elementos()
                 self.__updater.update_partida(self.jogador.velocidade.x)
                 jogador_group.draw(self.partida.tela)
-                self.__updater.update_jogador(self.partida.elementos, keys_pressed)
+                self.__updater.update_jogador(
+                    self.partida.elementos, keys_pressed)
 
             if keys_pressed[pygame.K_r]:
                 self.jogador.resetar()
--- versao_final/menuView.py
+++ versao_final/menuView.py
@@ -1,22 +1,54 @@
 import pygame
 from pygame.locals import *
+from botao_menu import Botao
+from filePaths import file_paths
 
 
 class menuView():
 
     def __init__(self) -> None:
+        pygame.init()
+     #      fontesys60 = pygame.font.SysFont('calibri', 60)
+        fonte_botao = pygame.font.SysFont('calibri', 20)
         self.__tela = pygame.display.set_mode(
-            (800, 480))
+            (800, 500))
 
-    def desenha(self):
-        self.__tela.fill((0, 0, 0))
-        texto_mensagem = 'MENU'
-        texto_opcoes = "Aperte tab para iniciar, 'S' para mudar de skin e ESC para sair"
-        fontesys60 = pygame.font.SysFont('calibri', 60)
-        fontesys24 = pygame.font.SysFont('calibri', 24)
-        tela_texto_mensagem = fontesys60.render(
-            texto_mensagem, 1, (255, 255, 255))
-        tela_texto_opcoes = fontesys24.render(texto_opcoes, 1, (255, 255, 255))
-        self.__tela.blit(tela_texto_mensagem, (240, 200))
-        self.__tela.blit(tela_texto_opcoes, (220, 280))
+        self.fundo_menu = pygame.image.load(
+            f'{file_paths.imagens}/menu_view/fundo_menu.png')
+        self.fundo_menu = pygame.transform.smoothscale(
+            self.fundo_menu.convert(), (800, 480))
+
+        self.fundo_botao = pygame.image.load(
+            f'{file_paths.imagens}/menu_view/ret_menu.png')
+
+        self.fundo_botao = pygame.transform.smoothscale(
+            self.fundo_botao.convert(), (100, 30))
+
+        self.lista_botoes = [
+            Botao(imagem=self.fundo_botao, x_pos=250, y_pos=350, mensagem='Jogar',
+                  fonte=fonte_botao, cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+            Botao(imagem=self.fundo_botao, x_pos=360, y_pos=350, mensagem='Skins',
+                  fonte=fonte_botao, cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+            Botao(imagem=self.fundo_botao, x_pos=470, y_pos=350, mensagem='Instruções',
+                  fonte=fonte_botao, cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+            Botao(imagem=self.fundo_botao, x_pos=580, y_pos=350, mensagem='Sair',
+                  fonte=fonte_botao, cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6))
+        ]
+
+        # BOTÕES: jogar, skins, instrucoes, sair
+
+    '''param :mouse_pos: entregue pelo pygame dentro do loop'''
+
+    def desenha(self, mouse_pos):
+        self.__tela.blit(self.fundo_menu, (0, 0))
+
+        for botao in self.lista_botoes:
+            botao.update(self.__tela)
+            botao.muda_cor(mouse_pos)
+
+
+#      self.__tela.blit(tela_texto_opcoes, (220, 280))
         pygame.display.update()  # tela
+
+        # se for True, o mouse esta acima do botao. me da (True, botao)
+        # eu vou usar isso caso eu receba um click na tela. pego botao.mensagem para ver que func vou executar
