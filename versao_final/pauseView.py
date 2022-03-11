320c5c4de2add16e3bf7f6bdf075f7708d4d09fb
Larissa Justen <larissajusten@gmail.com>
2022-03-11 10:58:06 -0300

    Add game pause

---
+++ versao_final/pauseView.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.locals import *
+from botao_menu import Botao
+from filePaths import file_paths
+
+
+class pauseView():
+
+    def __init__(self) -> None:
+        fonte_botao = pygame.font.SysFont('calibri', 20)
+        self.tela = pygame.Surface([400, 200], SRCALPHA, 32)
+        self.tela = self.tela.convert_alpha()
+        self.tela.fill((255, 255, 255, 95))
+
+        texto_mensagem = 'Jogo pausado'
+        fontesys24 = pygame.font.SysFont('calibri', 24)
+        self.tela_texto_mensagem = fontesys24.render(
+            texto_mensagem, 1, (0, 0, 0))
+
+        self.fundo_botao = self.__adiciona_e_transforma_imagem(
+            'ret_menu.png', 100, 30)
+
+        self.lista_botoes = [
+            Botao(imagem=self.fundo_botao, x_pos=290, y_pos=280,
+                  mensagem='Voltar', fonte=fonte_botao,
+                  cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+            Botao(imagem=self.fundo_botao, x_pos=400, y_pos=280,
+                  mensagem='Menu', fonte=fonte_botao,
+                  cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+            Botao(imagem=self.fundo_botao, x_pos=510, y_pos=280,
+                  mensagem='Sair', fonte=fonte_botao,
+                  cor_base_texto=(255, 255, 255), cor_mouse=(255, 137, 6)),
+        ]
+
+    def __adiciona_e_transforma_imagem(self, file_name, size_x, size_y):
+        image = pygame.image.load(
+            f'{file_paths.imagens}/menu_view/{file_name}')
+        return pygame.transform.smoothscale(
+            image.convert(), (size_x, size_y))
+
+    def desenha(self, tela):
+        for botao in self.lista_botoes:
+            botao.update(tela)
+            botao.muda_cor(pygame.mouse.get_pos())
+
+        tela.blit(self.tela_texto_mensagem, (330, 200))
+
+        pygame.display.update()
+
+    def removeView(self, tela):
+        self.tela.fill(0, 0, 0, 0)
