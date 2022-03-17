5e88392416234d17a0d59df05c20adb20e118f0f
FKettl <fekettl@gmail.com>
2022-03-17 16:32:02 -0300

    correção dos bg no figma

--- versao_final/containerFases.py
+++ versao_final/containerFases.py
@@ -10,12 +10,12 @@ class ContainerFases():
             Fase('Fase 1',
                  f'{file_paths.musicas}/undertale-megalovania.mp3',
                  f'{file_paths.mapas}/mapa_teste4.json',
-                 f'{file_paths.imagens}/bg_green.png',
-                 f'{file_paths.imagens}/floor_green.png'),
+                 f'{file_paths.imagens}/bg_blue.png',
+                 f'{file_paths.imagens}/floor_blue.png'),
             Fase('Fase 2',
                  f'{file_paths.musicas}/music_bossfight-Vextron.mp3',
                  f'{file_paths.mapas}/teste_voo.json',
-                 f'{file_paths.imagens}/bg2_1.jpg',
-                 f'{file_paths.imagens}/floor2.png', volume=0.2),
+                 f'{file_paths.imagens}/bg_purple.jpg',
+                 f'{file_paths.imagens}/floor_purple.png', volume=0.2),
 
         ]
